ec6f6f9fd5e7a2d2f04b4b11eadc0ed4980b2cf8
sunqian_sr <sunqian_sr@neusiri.com>
2022-07-04 17:35:15 +0800

    first-commit

---
+++ fgap_scaner/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+		<router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
